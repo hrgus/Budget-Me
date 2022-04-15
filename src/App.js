b33d6580c43163f9cdc64a03120220cfb4207e7e
Harry <hrgus3@gmail.com>
2022-04-15 10:53:46 -0400

    adds Switch hook to the App.js Component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import './App.css';
+import React, { useState } from 'react';
+import { Switch, Route } from 'react-router-dom';
+import Overview from './Components/Overview';
 import BudgetPage from './Components/BudgetPage';
 import NavBar from './Components/NavBar';
 import SavingsPage from './Components/SavingsPage';
@@ -7,9 +10,26 @@ import TransactionsList from './Components/TransactionsList';
 
 function App() {
   return (
-    <div className="App">
-      <h1> Hello </h1>
-    </div>
+    <Switch>
+      <Route path="/overview">
+        <Overview />
+      </Route>
+      <Route path="/BudgetPage">
+        <BudgetPage />
+      </Route>
+      <Route path="/NavBar">
+        <NavBar />
+      </Route>
+      <Route path="/SavingsPage">
+        <SavingsPage />
+      </Route>
+      <Route path="/TransactionItem">
+        <TransactionItem />
+      </Route>
+      <Route path="/TransactionsList">
+        <TransactionsList />
+      </Route>
+    </Switch>
   );
 }
 
---
+++ src/Components/Overview.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Overview() {
+
+   return (
+      <div>
+         <h1> Overview </h1>
+      </div>
+   )
+}
+
+export default Overview;
