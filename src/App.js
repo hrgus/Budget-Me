9def36ef853093b003a1ce7cb6519bab766d58ee
Harry <hrgus3@gmail.com>
2022-04-16 20:17:11 -0400

    Adds the local project to this GitHub Repo.

--- src/App.js
+++ src/App.js
@@ -1,37 +1,37 @@
-import './App.css';
-import React, { useState } from 'react';
-import { Switch, Route } from 'react-router-dom';
-import Overview from './Components/Overview';
-import BudgetPage from './Components/BudgetPage';
-import NavBar from './Components/NavBar';
-import SavingsPage from './Components/SavingsPage';
-import TransactionItem from './Components/TransactionItem';
-import TransactionsList from './Components/TransactionsList';
+import "./App.css";
+import React, { useState } from "react";
+import { Switch, Route } from "react-router-dom";
+import Overview from "./Components/Overview";
+import BudgetPage from "./Components/BudgetPage";
+import NavBar from "./Components/NavBar";
+import SavingsPage from "./Components/SavingsPage";
+import TransactionItem from "./Components/TransactionItem";
+import TransactionsList from "./Components/TransactionsList";
 
 function App() {
   return (
-    <Switch>
-      <Route path="/overview">
-        <Overview />
-      </Route>
-      <Route path="/BudgetPage">
-        <BudgetPage />
-      </Route>
-      <Route path="/NavBar">
-        <NavBar />
-      </Route>
-      <Route path="/SavingsPage">
-        <SavingsPage />
-      </Route>
-      <Route path="/TransactionItem">
-        <TransactionItem />
-      </Route>
-      <Route path="/TransactionsList">
-        <TransactionsList />
-      </Route>
-    </Switch>
+    <div>
+      <NavBar />
+      <Switch>
+        <Route path="/overview">
+          <Overview />
+        </Route>
+        <Route path="/BudgetPage">
+          <BudgetPage />
+        </Route>
+
+        <Route path="/SavingsPage">
+          <SavingsPage />
+        </Route>
+        <Route path="/TransactionItem">
+          <TransactionItem />
+        </Route>
+        <Route path="/TransactionsList">
+          <TransactionsList />
+        </Route>
+      </Switch>
+    </div>
   );
 }
 
 export default App;
-
