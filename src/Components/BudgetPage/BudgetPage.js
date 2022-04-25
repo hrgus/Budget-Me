0372f047d802255fb8e78611d36e2cebd328e652
Harry <hrgus3@gmail.com>
2022-04-25 12:55:45 -0400

    Adds headers to BudgetPage.js, and adds some basic css to it's index.css.

--- src/Components/BudgetPage/BudgetPage.js
+++ src/Components/BudgetPage/BudgetPage.js
@@ -1,9 +1,16 @@
 import React from "react";
+import "./index.css";
 
 function BudgetPage() {
   return (
     <div>
       <h1> BudgetPage </h1>
+      <div className="incomeListDiv">
+        <h3> Income </h3>
+      </div>
+      <div className="expenseListDiv">
+        <h3> Expenses </h3>
+      </div>
     </div>
   );
 }
