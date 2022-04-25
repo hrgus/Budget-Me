42cc63a7f2d1ca7609fb40ebce8b8a848e513c33
Harry <hrgus3@gmail.com>
2022-04-25 12:39:04 -0400

    Moves the fetches from Overview.js to App.js.

--- src/App/App.js
+++ src/App/App.js
@@ -1,20 +1,54 @@
 import "./App.css";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Switch, Route } from "react-router-dom";
 import Overview from "../Components/Overview/Overview";
 import BudgetPage from "../Components/BudgetPage/BudgetPage";
 import NavBar from "../Components/NavBar/NavBar";
 import SavingsPage from "../Components/SavingsPage/SavingsPage";
-import TransactionItem from "../Components/TransactionItem/TransactionItem";
-import TransactionsList from "../Components/TransactionsList/TransactionsList";
+// import TransactionItem from "../Components/TransactionItem/TransactionItem";
+// import TransactionsList from "../Components/TransactionsList/TransactionsList";
 
 function App() {
+  const [totalIncome, setTotalIncome] = useState([]);
+
+  // fetches
+  useEffect(() => {
+    // returns total expenses. Adds each expense.amount
+    let expenses = fetch("http://localhost:3000/expenses")
+      .then((resp) => resp.json())
+      .then((respObj) => {
+        // console.log(respObj);
+        let expenseAmount = respObj.reduce((aExpense, bExpense) => {
+          return aExpense.amount + bExpense.amount;
+        });
+        // console.log(expenseAmount);
+        return expenseAmount;
+      });
+    // returns total income. Adds each income.amount
+    let incomes = fetch("http://localhost:3000/income")
+      .then((resp) => resp.json())
+      .then((respObj) => {
+        // console.log(respObj);
+        let incomeAmount = respObj.reduce((aIncome, bIncome) => {
+          return aIncome.amount + bIncome.amount;
+        });
+        // console.log(incomeAmount);
+        return incomeAmount;
+      });
+    // awaits for the results of each fetch then subtract the total expenses from the total income
+    const incomeExpenses = async () => {
+      const a = await incomes;
+      const b = await expenses;
+      setTotalIncome(a - b);
+    };
+    return incomeExpenses;
+  }, []);
   return (
     <div>
       <NavBar />
       <Switch>
         <Route path="/overview">
-          <Overview />
+          <Overview totalIncome={totalIncome} />
         </Route>
         <Route path="/budget">
           <BudgetPage />
--- src/Components/Overview/Overview.js
+++ src/Components/Overview/Overview.js
@@ -1,42 +1,8 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 // import TransactionsList from "../TransactionsList/TransactionsList";
+// import BudgetPage from "../BudgetPage/BudgetPage";
 
-function Overview() {
-  const [totalIncome, setTotalIncome] = useState([]);
-
-  // fetches
-  useEffect(() => {
-    // returns total expenses. Adds each expense.amount
-    let expenses = fetch("http://localhost:3000/expenses")
-      .then((resp) => resp.json())
-      .then((respObj) => {
-        // console.log(respObj);
-        let expenseAmount = respObj.reduce((aExpense, bExpense) => {
-          return aExpense.amount + bExpense.amount;
-        });
-        // console.log(expenseAmount);
-        return expenseAmount;
-      });
-    // returns total income. Adds each income.amount
-    let incomes = fetch("http://localhost:3000/income")
-      .then((resp) => resp.json())
-      .then((respObj) => {
-        // console.log(respObj);
-        let incomeAmount = respObj.reduce((aIncome, bIncome) => {
-          return aIncome.amount + bIncome.amount;
-        });
-        // console.log(incomeAmount);
-        return incomeAmount;
-      });
-    // awaits for the results of each fetch then subtract the total expenses from the total income
-    const incomeExpenses = async () => {
-      const a = await incomes;
-      const b = await expenses;
-      setTotalIncome(a - b);
-    };
-    return incomeExpenses;
-  }, []);
-
+function Overview({ totalIncome }) {
   return (
     <div>
       <h1> Total Left To Budget </h1>
