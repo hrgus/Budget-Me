962340b3d43fbe314f5d17581c398c919a9dbeef
Harry <hrgus3@gmail.com>
2022-04-25 12:25:19 -0400

    fetches expenses.amount and adds each. subtracts expenses from income and updates state to that number.

--- src/Components/Overview/Overview.js
+++ src/Components/Overview/Overview.js
@@ -6,19 +6,40 @@ function Overview() {
 
   // fetches
   useEffect(() => {
-    fetch("http://localhost:3000/income")
+    // returns total expenses. Adds each expense.amount
+    let expenses = fetch("http://localhost:3000/expenses")
       .then((resp) => resp.json())
       .then((respObj) => {
+        // console.log(respObj);
+        let expenseAmount = respObj.reduce((aExpense, bExpense) => {
+          return aExpense.amount + bExpense.amount;
+        });
+        // console.log(expenseAmount);
+        return expenseAmount;
+      });
+    // returns total income. Adds each income.amount
+    let incomes = fetch("http://localhost:3000/income")
+      .then((resp) => resp.json())
+      .then((respObj) => {
+        // console.log(respObj);
         let incomeAmount = respObj.reduce((aIncome, bIncome) => {
           return aIncome.amount + bIncome.amount;
         });
-        setTotalIncome(incomeAmount);
+        // console.log(incomeAmount);
+        return incomeAmount;
       });
+    // supposed to wait for the results of each fetch then subtract the total expenses from the total income
+    const incomeExpenses = async () => {
+      const a = await incomes;
+      const b = await expenses;
+      setTotalIncome(a - b);
+    };
+    return incomeExpenses;
   }, []);
 
   return (
     <div>
-      <h1> Total Income </h1>
+      <h1> Total Left To Budget </h1>
       <h2> {totalIncome} </h2>
     </div>
   );
